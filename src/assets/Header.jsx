b99a4e13f5883812c916684038371a468a05a04c
FernandoVini <araujofernando592@gmail.com>
2025-09-14 09:57:05 -0300

    primeiro commit

---
+++ src/assets/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className='header'> 
+        <h1>escola</h1>
+        <input type="text" />
+        <ul className='ul'>
+            <li>conta</li>
+            <li>notocias</li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header

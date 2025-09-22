35d6fd68c63ad24650b93f2a39a4b306f68e906f
FernandoVini <araujofernando592@gmail.com>
2025-09-21 21:24:55 -0300

    deploy para vizualizacao

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { MantineProvider } from "@mantine/core" 
 import Conta from './pages/Conta.jsx'
 import Login from './pages/Login.jsx'
 import Aluno from './pages/Aluno.jsx'
@@ -11,24 +10,20 @@ import CriarLogin from './pages/CriarLogin.jsx'
 import LoginProf from './pages/LoginProf.jsx'
 import LoginCord from './pages/LoginCord.jsx'
 import LoginAluno from './pages/LoginAluno.jsx'
-import Coodenador from './pages/Coodenador.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <BrowserRouter>
-        <Routes>
-          <Route basename='/sistema-escolar-react' element={<App/>}/>
-          <Route path='/Conta' element={<Conta/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/aluno' element={<Aluno/>}/>
-          <Route path='/login2' element={<CriarLogin/>}/>
-          <Route path='/loginprof' element={<LoginProf/>} />
-          <Route path='/logincord' element={<LoginCord/>} />
-          <Route path='/loginaluno' element={<LoginAluno/>} />
-          <Route path='/coordenador' element={<Coodenador/>} />
-        </Routes>
-      </BrowserRouter>
-    </MantineProvider>
+    <BrowserRouter basename="/sistema-escolar-react">
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/Conta' element={<Conta/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/aluno' element={<Aluno/>}/>
+        <Route path='/login2' element={<CriarLogin/>}/>
+        <Route path='/loginprof' element={<LoginProf/>} />
+        <Route path='/logincord' element={<LoginCord/>} />
+        <Route path='/loginaluno' element={<LoginAluno/>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
